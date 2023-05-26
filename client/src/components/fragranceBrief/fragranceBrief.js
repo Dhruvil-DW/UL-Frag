fb4a9be948e38e0951140ee5d646d4ef04b1fcde
Dhruvil <support@digiwagon.com>
2023-05-26 17:01:48 +0530

    "Navigation added"

--- client/src/components/fragranceBrief/fragranceBrief.js
+++ client/src/components/fragranceBrief/fragranceBrief.js
@@ -3,8 +3,10 @@ import { Button} from '@mui/material'
 import UnileverIcon from "../../assets/icons/unileverIcon";
 import ArrowIcon from "../../assets/icons/arrowleft";
 import CategoryIcon from "../../assets/icons/categoryIcon";
+import { useNavigate } from "react-router-dom";
 export default function FragranceBrief() {
 
+  const navigate = useNavigate()
   return (
     <div className="overviewWrapper"style={{ backgroundImage: 'url("images/overview_image.png")' }}>
       <div className="overviewContainer">
@@ -12,10 +14,10 @@ export default function FragranceBrief() {
           <UnileverIcon/>
         </div>
         <div className="commonContainer">
-        <Button variant="contained" color="secondary" style={{marginTop: 64, marginLeft:123}}><ArrowIcon/><CategoryIcon/>Dashboard
+        <Button variant="contained" color="secondary" style={{marginTop: 64, marginLeft:123}} onClick={() => navigate("/dashboard")}><ArrowIcon/><CategoryIcon/>Dashboard
         </Button>
         <h1 style={{textAlign: 'left', paddingTop: '18vw', margin: 0, paddingLeft: 121, color: '#002F98'}}>Give us an overview of<br/>your fragrance brief</h1>
-        <Button variant="contained" color="secondary" className="get_started" style={{marginLeft: 117, marginTop:12}}>Get started</Button>
+        <Button variant="contained" color="secondary" className="get_started" style={{marginLeft: 117, marginTop:12}}  onClick={() => navigate("new")}>Get started</Button>
         </div>
       </div>
     </div>

3380aafce5bc2e5474e2527299c8d7b19d22864f
dhruvi009 <dhruvi@digiwagon.com>
2023-06-15 11:04:20 +0530

    changes in countries and regions seeders

--- server/seeders/20230525120057-country.js
+++ server/seeders/20230525120057-country.js
@@ -2,16 +2,24 @@
 
 /** @type {import('sequelize-cli').Migration} */
 const countries = [
-  { name: 'Argentina', region_id: 1 },
-  { name: 'Brazil', region_id: 1 },
-  { name: 'Colombia', region_id: 1 },
-  { name: 'India', region_id: 2 },
-  { name: 'Singapore', region_id: 2 },
-  { name: 'Malaysia', region_id: 2 },
+  { name: 'Argentina', region_id: 2 },
+  { name: 'Australia', region_id: 3 },
+  { name: 'Brazil', region_id: 2 },
+  { name: 'Bangladesh', region_id: 7 },
+  { name: 'China', region_id: 5 },
   { name: 'France', region_id: 3 },
+  { name: 'India', region_id: 7 },
+  { name: 'Indonesia', region_id: 4 },
+  { name: 'Italy', region_id: 3 },
   { name: 'Netherlands', region_id: 3 },
+  { name: 'Pakistan', region_id: 7 },
+  { name: 'Philippines', region_id: 6 },
+  { name: 'Poland', region_id: 3 },
+  { name: 'South Africa', region_id: 1 },
+  { name: 'Thailand', region_id: 6 },
   { name: 'Turkey', region_id: 3 },
   { name: 'United Kingdom', region_id: 3 },
+  { name: 'Vietnam', region_id: 6 },
 ]
 module.exports = {
   async up(queryInterface, Sequelize) {
--- server/seeders/20230608044319-region.js
+++ server/seeders/20230608044319-region.js
@@ -2,9 +2,13 @@
 
 /** @type {import('sequelize-cli').Migration} */
 const regions = [
-  {region_name: 'LATAM'},
+  {region_name: 'Africa'},
+  {region_name: 'Americas'},
+  {region_name: 'Europe'},
+  {region_name: 'Indonesia'},
+  {region_name: 'North Asia'},
   {region_name: 'SEAA'}, 
-  {region_name: 'EUROPE'},
+  {region_name: 'South Asia'},
 ]
 module.exports = {
   async up (queryInterface, Sequelize) {
